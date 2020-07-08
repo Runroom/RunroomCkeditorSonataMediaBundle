3d9f0f89b72bbf61b921f6f84dd95186a5ae84ef
Jordi Sala Morales <jordism91@gmail.com>
2020-07-08 10:20:11 +0200

    add missing test (#63)

---
+++ tests/App/Kernel.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Runroom package.
+ *
+ * (c) Runroom <runroom@runroom.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Runroom\CkeditorSonataMediaBundle\Tests\App;
+
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use Knp\Bundle\MenuBundle\KnpMenuBundle;
+use Runroom\CkeditorSonataMediaBundle\RunroomCkeditorSonataMediaBundle;
+use Runroom\CkeditorSonataMediaBundle\Tests\App\Entity\Media;
+use Sonata\AdminBundle\SonataAdminBundle;
+use Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle;
+use Sonata\MediaBundle\SonataMediaBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Bundle\SecurityBundle\SecurityBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+
+    public function registerBundles(): iterable
+    {
+        return [
+            new DoctrineBundle(),
+            new FrameworkBundle(),
+            new KnpMenuBundle(),
+            new SecurityBundle(),
+            new SonataAdminBundle(),
+            new SonataDoctrineORMAdminBundle(),
+            new SonataMediaBundle(),
+            new TwigBundle(),
+
+            new RunroomCkeditorSonataMediaBundle(),
+        ];
+    }
+
+    public function getCacheDir(): string
+    {
+        return $this->getBaseDir() . '/cache';
+    }
+
+    public function getLogDir(): string
+    {
+        return $this->getBaseDir() . '/log';
+    }
+
+    public function getProjectDir(): string
+    {
+        return __DIR__;
+    }
+
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
+    {
+        $container->loadFromExtension('framework', [
+            'test' => true,
+            'router' => ['utf8' => true],
+            'secret' => 'secret',
+        ]);
+
+        $container->loadFromExtension('security', [
+            'firewalls' => ['main' => ['anonymous' => true]],
+        ]);
+
+        $container->loadFromExtension('doctrine', [
+            'dbal' => ['url' => 'sqlite://:memory:', 'logging' => false],
+            'orm' => [
+                'auto_mapping' => true,
+            ],
+        ]);
+
+        $container->loadFromExtension('twig', [
+            'exception_controller' => null,
+            'strict_variables' => '%kernel.debug%',
+        ]);
+
+        $container->loadFromExtension('sonata_media', [
+            'default_context' => 'default',
+            'contexts' => ['default' => []],
+            'cdn' => null,
+            'db_driver' => 'doctrine_orm',
+            'class' => ['media' => Media::class],
+            'filesystem' => ['local' => null],
+        ]);
+    }
+
+    protected function configureRoutes(RouteCollectionBuilder $routes): void
+    {
+    }
+
+    private function getBaseDir(): string
+    {
+        return sys_get_temp_dir() . '/runroom-ckeditor-sonata-media-bundle/var';
+    }
+}
